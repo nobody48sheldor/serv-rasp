45846971d9230ae05368fd422803988254d7a4e2
nobody48sheldor <lolmyizi@gmail.com>
2022-01-07 15:51:38 +0100

    update input

--- backend.py
+++ backend.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+PATH = ""
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return(render_template("index.html"))
+
+@app.route("/browse", methods=["POST", "GET"])
+def browse():
+    if request.method == "POST":
+        path = request.form['files']
+        print(path)
+    return(render_template("browse.html"))
+
+@app.route("/file=<file>")
+def search(file):
+    return()
 
 if __name__ == "__main__":
     app.run()
