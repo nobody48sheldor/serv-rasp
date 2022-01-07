15f1342886ed6ba159423401226eee58366654bd
nobody48sheldor <lolmyizi@gmail.com>
2022-01-07 20:36:39 +0100

    update, working file browsing

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
+import os
 
+path = ["home", "arnaud", "Desktop", "arnaud"]
 PATH = ""
 
 app = Flask(__name__)
@@ -10,10 +12,38 @@ def home():
 
 @app.route("/browse", methods=["POST", "GET"])
 def browse():
+    dir = []
     if request.method == "POST":
-        path = request.form['files']
-        print(path)
-    return(render_template("browse.html"))
+        dir = []
+        folder = ""
+        try:
+            folder = request.form['files']
+        except:
+            None
+        if folder == "" :
+            folder = request.form['cliquedfile']
+        if folder == "..":
+            path.pop(len(path) - 1)
+            #return(render_template("index.html"))
+        else:
+            path.append(folder)
+        PATH = ""
+        for i in path:
+            PATH = PATH + "/" + i
+        try:
+            dir = os.listdir(PATH + "/")
+        except FileNotFoundError:
+            path.pop(len(path) - 1)
+        except NotADirectoryError:
+            path.pop(len(path) - 1)
+            return(render_template("index.html"))
+    else:
+        PATH = ""
+        for i in path:
+            PATH = PATH + "/" + i
+        dir = os.listdir(PATH + "/")
+    dir.append("..")
+    return(render_template("browse.html", files = dir, path = PATH))
 
 @app.route("/file=<file>")
 def search(file):
