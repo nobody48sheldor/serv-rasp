c0433e270dad32129d5073d799f24b6d4403ed0a
nobody48sheldor <lolmyizi@gmail.com>
2022-01-08 22:40:49 +0100

    upload and download file now possible

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, current_app
 import os
+from werkzeug.utils import secure_filename
+import shutil
 
 path = ["home", "arnaud", "Desktop", "arnaud"]
 PATH = ""
@@ -21,7 +23,30 @@ def browse():
         except:
             None
         if folder == "" :
-            folder = request.form['cliquedfile']
+            try:
+                folder = request.form['cliquedfile']
+            except:
+                None
+        if folder == "":
+            try:
+                savefile = request.files['upload']
+                PATH = ""
+                for i in path:
+                    PATH = PATH + "/" + i
+                savefile.save(secure_filename(savefile.filename))
+                dest = shutil.move(savefile.filename, PATH, copy_function = shutil.copytree)
+            except:
+                None
+        if folder == "":
+            try:
+                Dfile = request.form['download']
+                PATH = ""
+                for i in path:
+                    PATH = PATH + "/" + i
+                #send_from_directory(directory=PATH, path=Dfile)
+                return(send_from_directory(directory=PATH, path=Dfile, as_attachment=True, max_age=0))
+            except:
+                None
         if folder == "..":
             path.pop(len(path) - 1)
             #return(render_template("index.html"))
@@ -43,11 +68,12 @@ def browse():
             PATH = PATH + "/" + i
         dir = os.listdir(PATH + "/")
     dir.append("..")
-    return(render_template("browse.html", files = dir, path = PATH))
+    print(PATH)
+    return(render_template("browse.html", files = dir, path = PATH, d = False))
 
 @app.route("/file=<file>")
 def search(file):
     return()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
